d6fa68ca6ebe53e889f4e7f32461046a44bdbdb7
technodumb <65112984+technodumb@users.noreply.github.com>
2021-05-20 22:33:08 +0530

    Create DIV03.cpp

---
+++ CodeChef/DIV03.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int dif[10], k, sum=0;
+
+        for(int i=0;i<10;i++){
+            cin>>dif[i];
+        }
+        cin>>k;
+
+        for(int i=9;i>=0;i--){
+            sum+=dif[i];
+            if(sum>k){
+                cout<<i+1<<endl;
+                break;
+            }
+        }
+    }
+}
