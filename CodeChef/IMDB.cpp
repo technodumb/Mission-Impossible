e6cb64d53a7ff37a71e848b9b3cd87253a56807f
Alnas Kabeer <alnaskabeer@gmail.com>
2021-05-21 21:54:21 +0530

    added comments

--- CodeChef/IMDB.cpp
+++ CodeChef/IMDB.cpp
@@ -3,15 +3,17 @@ using namespace std;
 
 int main(){
     int T;
-    cin>>T;
+    cin>>T;                                 //Inputting number of test cases
     while(T--){
-        int n,x, max=-1, maxind = -1;
-        cin>>n>>x;
+        int n,x;
+        int max=-1, maxind = -1;
+        cin>>n>>x;                          //Input - number of movies and maximum size in harddisk      
         int spr, rating;
         for(int i=0;i<n;i++){
-            cin>>spr>>rating;
-            if(spr <= x && rating > max)
-                max = rating;
+            cin>>spr>>rating;               //Inputting the space required by the movie and its imdb rating
+            if(spr <= x && rating > max)    // Checking if the movie fits in the harddisk 
+                                            // and if the rating of the current movie is more than the maximum as of yet
+                max = rating;               //If you found a movie that fits in, store its rating in max, for future comparision.
         }
         cout<<max<<endl;
     }
