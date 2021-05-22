d1088f02d939aeb4210d9076c85c7de734b692d4
Alnas Kabeer <alnaskabeer@gmail.com>
2021-05-22 20:54:50 +0530

    removed a brace

--- CodeChef/T20MCH.cpp
+++ CodeChef/T20MCH.cpp
@@ -4,9 +4,8 @@ using namespace std;
 int main(){
     int R, O, C;
     cin>>R>>O>>C;
-    if (R-C < (20-O)*36){
+    if (R-C < (20-O)*36)
         cout<<"YES"<<endl;
-    }
     else
         cout<<"NO"<<endl;
     return 0;
