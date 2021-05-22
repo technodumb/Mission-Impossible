3d12afd47b55ae85cad5e1b7584f7b8872eadfe6
Alnas Kabeer <alnaskabeer@gmail.com>
2021-05-22 20:52:43 +0530

    solved T20MCH from codechef

--- CodeChef/IMDB.cpp
+++ CodeChef/IMDB.cpp
@@ -6,14 +6,14 @@ int main(){
     cin>>T;                                 //Inputting number of test cases
     while(T--){
         int n,x;
-        int max=-1, maxind = -1;
+        int max=-1;
         cin>>n>>x;                          //Input - number of movies and maximum size in harddisk      
         int spr, rating;
         for(int i=0;i<n;i++){
             cin>>spr>>rating;               //Inputting the space required by the movie and its imdb rating
             if(spr <= x && rating > max)    // Checking if the movie fits in the harddisk 
                                             // and if the rating of the current movie is more than the maximum as of yet
-                max = rating;              //If you found a movie that fits in, store its rating in max, for future comparision.
+                max = rating;               //If you found a movie that fits in, store its rating in max, for future comparision.
         }
         cout<<max<<endl;
     }
---
+++ CodeChef/T20MCH.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int R, O, C;
+    cin>>R>>O>>C;
+    if (R-C < (20-O)*36){
+        cout<<"YES"<<endl;
+    }
+    else
+        cout<<"NO"<<endl;
+    return 0;
+}
