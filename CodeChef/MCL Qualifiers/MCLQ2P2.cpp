39964c562ae79acd466d70bb499200edfed3ac0e
Alnas Kabeer <alnaskabeer@gmail.com>
2021-05-27 23:22:02 +0530

    modification

--- CodeChef/MCL Qualifiers/MCLQ2P2.cpp
+++ CodeChef/MCL Qualifiers/MCLQ2P2.cpp
@@ -6,15 +6,18 @@ int main(){
     cin>>t;
     while(t--){
         int a[2], b[2], c[2];
-        int coinc=0;
+        bool flag = true;
         cin>>a[0]>>a[1]>>b[0]>>b[1]>>c[0]>>c[1];
+
         if(a[0]<=b[1] && b[0]<=a[1])
             if(b[0]<=c[1] && c[0]<=b[1])
-                if(c[0]<=a[1] && a[0]<=c[1]){
+                if(c[0]<=a[1] && a[0]<=c[1])
+                
+                {
                     cout<<"WIN"<<endl;
-                    coinc = 1;
+                    flag = false;
                     }
-        if (coinc==0)
+        if (flag)
             cout<<"LOSE"<<endl;
     }
     return 0;
