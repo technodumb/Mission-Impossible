52b5d5185f0b3b87afd5b43c6d6a05623e963baf
Alnas Kabeer <alnaskabeer@gmail.com>
2021-06-19 17:14:52 +0530

    it worked lol

---
+++ Codeforces/18-June/b.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        long long int m,n,x,y;
+        cin>>m>>n>>x>>y;
+        cout<<"1 1 "<<m<<" "<<n<<endl;
+    }
+    return 0;
+}
