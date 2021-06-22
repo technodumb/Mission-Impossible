4703837c61db84ed1774c8a57beb5e0e3b3281d5
Alnas Kabeer <alnaskabeer@gmail.com>
2021-06-23 00:57:00 +0530

    day 19

---
+++ Hackerrank/Code Actually/day19.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+int main() {
+    int n, a, b;
+    string str;
+    cin>>n>>str>>a>>b;
+    while(n--){
+        int sar=0, anu=0;
+        string ran;
+        cin>>ran;
+        for(auto i:ran)
+            if(str.find(i)<str.length())
+                sar+=a;
+            else anu+=b;
+        if(sar>anu)
+            cout<<"SARTHAK";
+        else if(sar == anu)
+            cout<<"DRAW";
+        else cout<<"ANURADHA";
+        cout<<"\n";
+    }
+    return 0;
+}
