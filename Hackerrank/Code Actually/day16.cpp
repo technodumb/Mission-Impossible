df4cb2839768f6a06edc9f8640d0ec35aa1a3585
Alnas Kabeer <alnaskabeer@gmail.com>
2021-06-24 22:35:53 +0530

    day 16 redefined

--- Hackerrank/Code Actually/day16.cpp
+++ Hackerrank/Code Actually/day16.cpp
@@ -1,27 +1,31 @@
-#include<iostream>
+#include <iostream>
+#include <string>
 using namespace std;
-
-int main(){
-    string str;
-    cin>>str;
-    for(int i=0;i<str.length();i++){
-        for(int j=0;j<str.length();j++){
-            if(i==j)
-                continue;
-            for(int k=0;k<str.length();k++){
-                if(k==j || k==i)
-                    continue;
-                for(int l=0;l<5;l++){
-                    if(l==k || l==j|| l==i)
-                        continue;
-                    for(int m=0;m<5;m++){
-                        if(m==l || m==k || m==j|| m==i)
-                            continue;
-                        cout<<str[i]<<str[j]<<str[k]<<str[l]<<str[m]<<"\n";    
-                    }
-                }
-            }
-        }
+ 
+void permute(string s , string answer)
+{
+    if(s.length() == 0)
+    {
+        cout<<answer<<"\n";
+        return;
     }
+    for(int i=0 ; i<s.length() ; i++)
+    {
+        char ch = s[i];
+        string left_substr = s.substr(0,i);
+        string right_substr = s.substr(i+1);
+        string rest = left_substr + right_substr;
+        permute(rest , answer+ch);
+    }
+ 
+}
+ 
+int main()
+{
+    string s;
+    string answer="";
+ 
+    cin>>s;
+    permute(s , answer);
     return 0;
 }
