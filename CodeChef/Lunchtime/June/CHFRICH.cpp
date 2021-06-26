6eadf05b6867b46b288c3bfc3aa1f7558898a38a
Alnas Kabeer <alnaskabeer@gmail.com>
2021-06-26 20:06:13 +0530

    lunchtime

---
+++ CodeChef/Lunchtime/June/CHFRICH.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    long long int t;
+    cin>>t;
+    while(t--){
+        int a,b,x;
+        cin>>a>>b>>x;
+        cout<<(b-a)/x<<"\n";
+    }
+}
